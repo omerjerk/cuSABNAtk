a643f3d44f4325bf0339987978106e455443b3c8
Umair <omerjerk@gmail.com>
2019-04-06 22:24:39 -0400

    allow benchmarking the queries
    Signed-off-by: Umair <omerjerk@gmail.com>

--- examples/query.cpp
+++ examples/query.cpp
@@ -3,11 +3,15 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <random>
+#include <algorithm>
 
 #include <jaz/logger.hpp>
 
 #include "GPUCounter.hpp"
 #include "csv.hpp"
+
+#define QUERY_TEST_COUNT 500
 
 struct Call {
     void init(int, int) { nijk = 0; }
@@ -18,7 +22,6 @@ struct Call {
 
     void operator()(int Nijk, int i) {
       nijk = Nijk;
-      printf("%d %d\n", nijk, i);
     }
 
     int score() const { return nijk; }
@@ -45,11 +48,31 @@ std::tuple<bool, std::vector<int>, std::vector<int>> read_query(std::string quer
     return std::make_tuple(true, pa, xi);
 }
 
+std::vector<std::vector<int>> get_benchmark_queries(int n, int nq) {
+    std::vector<int> variables(n);
+    for (int i = 0; i < n; ++i) {
+        variables[i] = i;
+    }
+    int seed = 100;
+    auto start = variables.begin();
+    auto end = start + nq;
+    std::vector<std::vector<int>> queries(QUERY_TEST_COUNT, std::vector<int>(nq));
+    for (int i = 0; i < QUERY_TEST_COUNT; ++i) {
+        shuffle(start, end, std::mt19937(seed));
+        for (int j = 0; j < nq; ++j) {
+            queries[i][j] = variables[j];
+        }
+    }
+    return queries;
+}
+
 int main(int argc, char* argv[]) {
+    using set_type = typename GPUCounter<2>::set_type;
     jaz::Logger Log;
 
-    if (argc != 3) {
-        std::cout << "usage: " << argv[0] << " data_file query_file" << std::endl;
+    if (argc < 3) {
+        std::cout << "Normal usage: " << argv[0] << " <data_file> <query_file>" << std::endl;
+        std::cout << "Benchmark usage: " << argv[0] << " <data_file> benchmark <number_of_variables_in_query>" << std::endl;
         return 0;
     }
 
@@ -70,27 +93,52 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    std::vector<int> paVec;
-    std::vector<int> xiVec;
-    std::tie(b, paVec, xiVec) = read_query(query_file);
-
     GPUCounter<2> gcount = create_GPUCounter<2>(n, m, std::begin(D));
 
-    using set_type = typename GPUCounter<2>::set_type;
-    auto xi = set_empty<set_type>();
-    auto pa = set_empty<set_type>();
+    if (query_file.compare("benchmark") == 0) {
+        //perform the benchmark
+        int nq = std::stoi(argv[3]);//number of variables in the query
+        if (n < nq) {
+            Log.error() << "Number of variables in query cannot be more than number of variables in the dataset" << std::endl;
+        }
+        double time = 0;
+        std::vector<Call> F(1);
+        auto queries = get_benchmark_queries(n, nq);
+        for (int i = 0; i < queries.size(); ++i) {
+            auto xi = set_empty<set_type>();
+            auto pa = set_empty<set_type>();
 
-    for (int i = 0; i < paVec.size(); ++i) {
-        pa = set_add(pa, paVec[i]);
+            xi = set_add(xi, queries[i][0]);
+            for (int j = 1; j < nq; ++j) {
+                pa = set_add(pa, queries[i][j]);
+                auto t0 = std::chrono::system_clock::now();
+                gcount.apply(xi, pa, F);
+                auto t1 = std::chrono::system_clock::now();
+                time += std::chrono::duration<double>(t1 - t0).count();
+            }
+        }
+        printf("Time for %d queries with %d variables = %f", QUERY_TEST_COUNT, nq, time);
+    } else {
+        //execute the query
+        std::vector<int> paVec;
+        std::vector<int> xiVec;
+        std::tie(b, paVec, xiVec) = read_query(query_file);
+
+        auto xi = set_empty<set_type>();
+        auto pa = set_empty<set_type>();
+
+        for (int i = 0; i < paVec.size(); ++i) {
+            pa = set_add(pa, paVec[i]);
+        }
+
+        for (int i = 0; i < xiVec.size(); ++i) {
+            xi = set_add(xi, xiVec[i]);
+        }
+
+        std::vector<Call> F(1);
+
+        gcount.apply(xi, pa, F);
     }
-
-    for (int i = 0; i < xiVec.size(); ++i) {
-        xi = set_add(xi, xiVec[i]);
-    }
-
-    std::vector<Call> F(1);
-
-    gcount.apply(xi, pa, F);
 
     return 0;
 } // main
