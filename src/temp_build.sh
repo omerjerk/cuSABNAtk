d35e9135aac86fc47467f92d1a209606d4dbd117
Umair <omerjerk@gmail.com>
2019-11-03 21:26:14 -0500

    CMake build broken, use custom script for now
    Signed-off-by: Umair <omerjerk@gmail.com>

---
+++ src/temp_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm gpu_util.o
+rm ../examples/query
+rm query.o
+
+#compile cuda code
+nvcc -arch=sm_61 -rdc=true -c gpu_util.cu -lcudadevrt -o gpu_util.o -I../include
+#create static library from object code
+ar rcs gpu_util.a gpu_util.o
+#compile C++ code
+g++ -fopenmp  -std=c++14 -O3 -c ../examples/query.cpp -o query.o -I/usr/local/cuda-10.0/include -I../include
+#link
+nvcc query.o gpu_util.a -Xcompiler -fopenmp  -std=c++14 -O3  -o ../examples/query \
+-I/usr/local/cuda-10.0/include -I../include /usr/local/cuda-10.0/lib64/libcudart_static.a \
+-ldl -lrt /usr/local/cuda-10.0/lib64/libcudart_static.a
