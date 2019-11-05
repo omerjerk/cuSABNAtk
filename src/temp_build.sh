45377e449299f35fec1d16477902f1f2fa793421
Umair <omerjerk@gmail.com>
2019-11-04 23:20:53 -0500

    fix build for ccr
    Signed-off-by: Umair <omerjerk@gmail.com>

--- src/temp_build.sh
+++ src/temp_build.sh
@@ -9,8 +9,8 @@ nvcc -arch=sm_61 -rdc=true -c gpu_util.cu -lcudadevrt -o gpu_util.o -I../include
 #create static library from object code
 ar rcs gpu_util.a gpu_util.o
 #compile C++ code
-g++ -fopenmp  -std=c++14 -O3 -c ../examples/query.cpp -o query.o -I/usr/local/cuda-10.0/include -I../include
+g++ -fopenmp  -std=c++14 -O3 -c ../examples/query.cpp -o query.o -I/util/common/cuda/cuda-10.0/include -I../include
 #link
 nvcc -arch=sm_61 query.o gpu_util.a -Xcompiler -fopenmp  -std=c++14 -O3  -o ../examples/query \
--I/usr/local/cuda-10.0/include -I../include /usr/local/cuda-10.0/lib64/libcudart_static.a \
--ldl -lrt /usr/local/cuda-10.0/lib64/libcudart_static.a
+-I/util/common/cuda/cuda-10.0/include -I../include /util/common/cuda/cuda-10.0/lib64/libcudart_static.a \
+-ldl -lrt /util/common/cuda/cuda-10.0/lib64/libcudart_static.a
